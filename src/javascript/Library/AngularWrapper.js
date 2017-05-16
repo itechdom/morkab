552c85351c1ff7463d6e887f53a7bbd8df83fc13
Sam Alghanmi <oalghnmi@gmail.com>
2017-05-16 15:44:50 -0400

    Removing extra angular wrapper

--- src/javascript/Library/AngularWrapper.js
+++ src/javascript/Library/AngularWrapper.js
@@ -15,6 +15,14 @@ export class AngularWrapper extends React.Component{
   constructor(props){
     super(props);
   }
+  refreshAngular(){
+    let tags = ["inline.bundle.js","polyfills.bundle.js","styles.bundle.js","vendor.bundle.js","main.bundle.js"];
+    tags.map((tag)=>{
+      let my_awesome_script = document.createElement('script');
+      my_awesome_script.setAttribute('src',`AngularApp/dist/${tag}`);
+      document.body.appendChild(my_awesome_script);
+    })
+  }
   render(){
     const { componentList, itemType, handlePageComponentDrag, handleComponentEdit, children, id, subChildren, store, comp, selector} = this.props;
     const angularComponent = `<my-app [name]="hello"></my-app>`;
@@ -23,7 +31,7 @@ export class AngularWrapper extends React.Component{
       key={id}
       ref={(domElement) => {console.log(domElement)}}
       >
-    </div>;
-  }
+      </div>;
+    }
 
-}
+  }
