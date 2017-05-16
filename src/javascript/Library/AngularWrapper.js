430a3fdf6a0c9eaae84feefaf8e218eb41949bf6
Sam Alghanmi <oalghnmi@gmail.com>
2017-05-16 11:45:48 -0400

    angular wrapper

--- src/javascript/Library/AngularWrapper.js
+++ src/javascript/Library/AngularWrapper.js
@@ -7,70 +7,6 @@ import {
 }
 from "mobx-react";
 
-/**
-* Specifies the drop target contract.
-* All methods are optionail.
-*/
-const boardTarget = {
-  canDrop(props, monitor) {
-    // You can disallow drop based on props or item
-    const item = monitor.getItem();
-    return true;
-  },
-
-  hover(props, monitor, component) {
-    // This is fired very often and lets you perform side effects
-    // in response to the hover. You can't handle enter and leave
-    // here—if you need them, put monitor.isOver() into collect() so you
-    // can just use componentWillReceiveProps() to handle enter/leave.
-
-    // You can access the coordinates if you need them
-    const clientOffset = monitor.getClientOffset();
-    //const componentRect = findDOMNode(component).getBoundingClientRect();
-
-    //props.handleComponentHover(clientOffset);
-
-    let item = monitor.getItem();
-    let itemType = monitor.getItemType();
-    // You can check whether we're over a nested drop target
-    const isJustOverThisOne = monitor.isOver({ shallow: true });
-
-    // You will receive hover() even for items for which canDrop() is false
-    const canDrop = monitor.canDrop();
-  },
-
-  drop(props, monitor, component) {
-    if (monitor.didDrop()) {
-      // If you want, you can check whether some nested
-      // target already handled drop
-      return;
-    }
-
-    // Obtain the dragged item
-    const item = monitor.getItem();
-    const itemType = monitor.getItemType();
-
-    props.store.addItemToComponent(item,props.comp);
-
-    // You can also do nothing and return a drop result,
-    // which will be available as monitor.getDropResult()
-    // in the drag source's endDrag() method
-    return { moved: true };
-  }
-};
-
-@DropTarget(['generalcomponent','pagecomponent'], boardTarget, (connect, monitor) => {
-  return {
-    // Call this function inside render()
-    // to let React DnD handle the drag events:
-    connectDropTarget: connect.dropTarget(),
-    // You can ask the monitor about the current drag state:
-    isOver: monitor.isOver(),
-    isOverCurrent: monitor.isOver({ shallow: true }),
-    canDrop: monitor.canDrop(),
-    itemType: monitor.getItemType()
-  }
-})
 @observer
 export class AngularWrapper extends React.Component{
   static propTypes = {
@@ -80,14 +16,13 @@ export class AngularWrapper extends React.Component{
     super(props);
   }
   renderAngular(selector){
-      //bootstrap
+    //bootstrap
   }
   render(){
-    const { isOver, canDrop, connectDropTarget, componentList, itemType, handlePageComponentDrag, handleComponentEdit, children, id, subChildren, store, comp, selector} = this.props;
-    let minHeight;
+    const { componentList, itemType, handlePageComponentDrag, handleComponentEdit, children, id, subChildren, store, comp, selector} = this.props;
     let Arr = subChildren.map((Child,index)=>{
       let flexStyle = (Child.properties.style && Child.properties.style.flex)?Child.properties.style.flex:0;
-      return <div style={{flex:flexStyle,animationDuration: "1s",animationFillMode: "both"}}>
+      return <div style={{flex:flexStyle}}>
         <Child.element
           key={Child.id}
           id={Child.id}
@@ -101,10 +36,9 @@ export class AngularWrapper extends React.Component{
         <RaisedButton style={{float:'right',zIndex:999}} label="Edit" onClick={()=>handleComponentEdit(Child)} />
       </div>
     });
-    (subChildren.length>0)?minHeight="0px":minHeight="100px";
-    return connectDropTarget(<div key={id} style={{display:'flex', flexDirection:'column', border:`2px solid black`, minHeight:`${minHeight}`}}>
-            {Arr}
-      </div>);
-    }
-
+    return <div key={id} style={{display:'flex', flexDirection:'column', border:`2px solid black`, minHeight:`${minHeight}`}}>
+      {Arr}
+    </div>;
   }
+
+}
---
+++ src/javascript/angular-component-list.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import * as Material from 'material-ui';
+
+const docWebsite = "https://material.angular.io/components/";
+
+const listData = [
+  "item 1",
+  "item 2",
+  "item 3"
+];
+
+const iconData = [
+  "restore",
+  "favorite"
+]
+
+export default [
+  {
+    "element":Animation,
+    "title":"Animation",
+    "link":`${docWebsite}toolbar`,
+    "properties":{
+      children:[]
+    },
+    subChildren:[]
+  }
+];
