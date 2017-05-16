b2c3d8e6b3f9a800838896a7156e5188992d3d8f
Sam Alghanmi <oalghnmi@gmail.com>
2017-05-16 14:36:53 -0400

    setting html dangerously

--- src/javascript/Library/AngularWrapper.js
+++ src/javascript/Library/AngularWrapper.js
@@ -16,12 +16,12 @@ export class AngularWrapper extends React.Component{
     super(props);
   }
   renderAngular(selector){
-    //bootstrap angular? or just change the input value and let the angualr app do the rest?
+    //change the input of the box here?
   }
   render(){
     const { componentList, itemType, handlePageComponentDrag, handleComponentEdit, children, id, subChildren, store, comp, selector} = this.props;
-    return <div key={id}>
-      <my-app></my-app>
+    const angularComponent = `<my-app [selector]=${selector}></my-app>`
+    return <div dangerouslySetInnerHTML={{__html: angularComponent}} key={id}>
     </div>;
   }
 
