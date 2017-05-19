746c912a81f74ddba97afd9709eacdc50613a634
Sam Alghanmi <oalghnmi@gmail.com>
2017-05-19 16:58:41 +0000

    added grommet to the registery

--- src/component-list/index.js
+++ src/component-list/index.js
@@ -1,8 +1,14 @@
 import {componentList as materialUI, wrapper as materialUIWrapper} from './libs/material-ui.js';
+import {componentList as grommetUI, wrapper as grommetWrapper} from './libs/grommet.js';
 export default [
   {
     name:"material",
     componentList:materialUI,
     wrapper:materialUIWrapper
+  },
+  {
+    name:"grommet",
+    componentList:grommetUI,
+    wrapper:grommetWrapper
   }
 ]
