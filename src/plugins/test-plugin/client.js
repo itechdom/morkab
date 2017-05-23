99c86be004f5beb85386a9bb5ee38751832e1ebe
Sam Alghanmi <oalghnmi@gmail.com>
2017-05-23 14:56:03 +0000

    added plugins feture

---
+++ src/plugins/test-plugin/client.js
@@ -0,0 +1,6 @@
+export default function testPluginClient({
+    renderer
+}){
+    //write your view here and call the appropriate renderer: React, Angular, Vue ... etc
+    //we will use React for now
+}
---
+++ src/plugins/test-plugin/server.js
@@ -0,0 +1,10 @@
+//import other plugins here if you would like
+
+export default function testPluginServer({
+    app,
+    config,
+    db,
+    apiRoutes
+}){
+    //add whatever you want here to extend the environment
+}
