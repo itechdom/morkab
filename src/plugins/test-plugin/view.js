8759f02e3d758ad96e0797c334f6971d823f31a0
Sam Alghanmi <oalghnmi@gmail.com>
2017-05-23 13:03:38 -0400

    restricting views to just themes

--- src/plugins/test-plugin/view.js
+++
@@ -1,9 +0,0 @@
-//import other libs here?
-export default function testPluginClient({
-    reactRenderer,
-    Material,
-    MaterialWrapper
-}){
-    //you can specify which section will be rendered here and which one is going to be rendered on the client
-    return <div>hello</div>;
-}
